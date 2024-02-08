dca371df00d7d119cceda280dded86b0de610906
zoppatorsk <49608884+zoppatorsk@users.noreply.github.com>
2024-02-08 21:44:05 +0100

    see readme for changs

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # isbn-search
 
-ISBN-search traverses a web page replacing isbn numbers with links to a gen.lib.rus.ec search for the isbn number. Not every textbook can be found online for free, but if the textbook can be found online, it will likely be on gen.lib.rus.ec
+ISBN-search traverses a web page replacing isbn numbers with links to a libgen search for the isbn number.
+Not every textbook can be found online for free, but if the textbook can be found online, it will likely be on libgen.
+
+## Update infromation
+
+Changed the libgen url to one that is working.
+Updated manifest to v3.
+Changed code to use const and let instead of var.
+Changed so the links create are opened in a new tab.
--- content_script.js
+++ content_script.js
@@ -1,63 +1,67 @@
+//new libgen adresses
+// https://libgen.is/ (Working – February 2024)
+// https://libgen.rs/ – (Working – February 2024)
+// https://libgen.st/ (Working August 2023)
 traverse(document.body);
 
 function traverse(node) {
-  var child, next
+	let child, next;
 
-  switch(node.nodeType) {
-  case 1:
-  case 9:
-  case 11:
-    child = node.firstChild;
-    while(child) {
-      next = child.nextSibling;
-      traverse(child);
-      child = next;
-    }
-    break;
-  case 3:
-    handleText(node);
-    break;
-  }
+	switch (node.nodeType) {
+		case 1:
+		case 9:
+		case 11:
+			child = node.firstChild;
+			while (child) {
+				next = child.nextSibling;
+				traverse(child);
+				child = next;
+			}
+			break;
+		case 3:
+			handleText(node);
+			break;
+	}
 }
 
 // Produces a link to a search for an isbn on gen.lib.rus.ec
 function generateBookLink(isbn) {
-  var url = "http://gen.lib.rus.ec/search.php?req=" + isbn + "&column=identifier\""
-  var hyperLink = document.createElement('a');
-  var linkText = document.createTextNode(isbn);
-  hyperLink.appendChild(linkText);
-  hyperLink.href = url;
-  return hyperLink;
+	const url = 'http://libgen.is/search.php?req=' + isbn + '&column=identifier"';
+	const hyperLink = document.createElement('a');
+	const linkText = document.createTextNode(isbn);
+	hyperLink.appendChild(linkText);
+	hyperLink.href = url;
+	hyperLink.target = '_blank';
+	return hyperLink;
 }
 
 // Function is called on every section of text
 function handleText(textNode) {
+	const isbnRegex = /(97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9])/g;
+	const textContents = textNode.nodeValue;
+	const regexResults = isbnRegex.exec(textContents);
 
-  var isbnRegex = /(97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9])/g;
-  var textContents = textNode.nodeValue;
-  var regexResults = isbnRegex.exec(textContents);
+	if (regexResults) {
+		// Determine the start and index of regex search
+		const startIndex = regexResults.index;
+		const endIndex = startIndex + regexResults[0].length;
 
-  if(regexResults) {
-    // Determine the start and index of regex search
-    var startIndex = regexResults.index;
-    var endIndex = startIndex + regexResults[0].length;
+		// Break up textNode contents into subparts
+		const part1 = textContents.substring(0, startIndex);
+		const part2 = generateBookLink(regexResults[0]);
+		const part3 = textContents.substring(endIndex);
 
-    // Break up textNode contents into subparts
-    var part1 = textContents.substring(0, startIndex);
-    var part2 = generateBookLink(regexResults[0]);
-    var part3 = textContents.substring(endIndex);
+		// console.log(part1);
+		// console.log(part3);
 
-    console.log(part1); console.log(part3);
-    
-    // Create text nodes for subparts
-    var part1Node = document.createTextNode(part1);
-    var part3Node = document.createTextNode(part3);
-    var parentNode = textNode.parentNode;
+		// Create text nodes for subparts
+		const part1Node = document.createTextNode(part1);
+		const part3Node = document.createTextNode(part3);
+		const parentNode = textNode.parentNode;
 
-    // Replace textNode with subparts
-    parentNode.replaceChild(part1Node, textNode);
-    parentNode.insertBefore(part2, part1Node.nextSibling);
-    parentNode.insertBefore(part3Node, part2.nextSibling);
-  }
-
+		// Replace textNode with subparts
+		parentNode.replaceChild(part1Node, textNode);
+		parentNode.insertBefore(part2, part1Node.nextSibling);
+		parentNode.insertBefore(part3Node, part2.nextSibling);
+	}
 }
